934a00ff556f67a4c31c6e8f7c037005d2d4cd74
Oles Marola <oles_pw@ukr.net>
2019-12-05 16:43:19 +0200

    Updates

---
+++ precache-manifest.d8ee90686ce36c0ce503e605b855bf4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89d3b74f28f38ae526d80962176bd569",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "8d6d12a1ddb2b869675f",
+    "url": "/react_tabs/static/css/main.6cb95dd7.chunk.css"
+  },
+  {
+    "revision": "477d3cfdba83b1e0f343",
+    "url": "/react_tabs/static/js/2.345f8e30.chunk.js"
+  },
+  {
+    "revision": "8d6d12a1ddb2b869675f",
+    "url": "/react_tabs/static/js/main.5d571773.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
